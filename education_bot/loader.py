56c791964ba0a128eea46ac3951c7867381d7913
Aqilbektech <seyilbekovaqilbek91@gmail.com>
2026-03-10 14:57:26 +0500

    Update loader.py

--- education_bot/loader.py
+++ education_bot/loader.py
@@ -1,5 +1,6 @@
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+bot = Bot(token="7874184131:AAEhXCgLj9KMlaqzFsCQ4KTuMFDyk_MjEA8", parse_mode="HTML")
 storage = MemoryStorage()
 dp=Dispatcher(bot,storage=MemoryStorage())
 
